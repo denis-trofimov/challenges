b4f09425cccbe59b3959f2a7949dcdcee97d695b
Denis Trofimov <silaradost@yandex.ru>
2019-09-24 19:33:02 +0300

    wip: frequency_fable_query timeouts 9-13.

---
+++ hackerrank/interviews/frequency_fable_query.py
@@ -0,0 +1,63 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+class FrequencyTable(dict):
+
+    def insert(self, value):
+        current = self.get(value)
+        if current:
+            self[value] = current + 1
+        else:
+            self[value] = 1
+
+    def remove(self, value):
+        current = self.get(value)
+        if current:
+            if current == 1:
+                self.pop(value)
+            else:
+                self[value] = current - 1
+
+    def check_exact_frequency(self, frequency):
+        if frequency in self.values():
+            return '1'
+        else:
+            return '0'
+
+# Complete the freqQuery function below.
+def freqQuery(queries):
+    frequency_fable = FrequencyTable()
+    answers = []
+    for query in queries:
+        op, value = query
+        if op == 1:
+            frequency_fable.insert(value)
+        elif op == 2:
+            frequency_fable.remove(value)
+        else:
+            answers.append(frequency_fable.check_exact_frequency(value))
+    return answers
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    queries = []
+
+    for _ in range(q):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    ans = freqQuery(queries)
+
+    fptr.write('\n'.join(map(str, ans)))
+    fptr.write('\n')
+
+    fptr.close()
